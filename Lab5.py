9182e0b61b96b416c58124d7cab1eabc9fa27d5d
shephatia-prog <shephatia1212@gmail.com>
2026-01-11 21:41:10 -0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -55,7 +55,7 @@ def centered_star_pyramid(n):
     for i in range(1, n + 1):
         spaces = n - i
         stars = 2 * i - 1
-        result += " " * spaces + "*"
+        result += " " * spaces + "*" * stars
         if i != n:
             result += "\n"
 
